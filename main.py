8b5420ea3c311b8ad2446a2fbdc6ebcc62f5fb28
SLTR <102388516+SLTR34@users.noreply.github.com>
2023-06-05 23:28:47 +0200

    arguments1

--- main.py
+++ main.py
@@ -1,43 +1,53 @@
 # Do not modify these lines
-__winc_id__ = '71dd124b4a6e4d268f5973db521394ee'
-__human_name__ = 'strings'
+__winc_id__ = '7b9401ad7f544be2a23321292dd61cb6'
+__human_name__ = 'arguments'
 
 # Add your code after this line
-# deel 1
-# doelpunten makers
-scorer_van_Basten= "Marco van Basten"
-scorer_Gullit = "Ruud Gullit"
 
-# minuten doelpunten
-goal_0 = 32
-goal_1 = 54
 
-# scoren verloop
-when_they_scored = [goal_0, goal_1]
+# Part 1: Greet template.
 
-scorers = f"{scorer_Gullit } {str (goal_0)}, { scorer_van_Basten } {str (goal_1)}"
+# Define a function greet in main.py that takes these arguments.
 
-report = f"{scorer_Gullit} scored in the {goal_0}nd minute\n{scorer_van_Basten} scored in the {goal_1}th minute"
+def greet(name, template = "Hello, <name>!"):
+    greet = template.replace("<name>", name)
+    return greet
 
-print (report)
+# Part 2: Force.
+mass=0
+def force(mass=0, body="earth"):
+    mass = int(mass)
+    body = str(body)
+    
+    planets = {
+        "mercury" : 3.7,
+        "venus" : 8.9, 
+        "earth" : 9.8, 
+        "moon" : 1.6,
+        "mars" : 3.7, 
+        "jupiter" : 23.1, 
+        "saturn" : 9,
+        "uranus" : 8.7,
+        "neptune" : 11,
+        "pluto" : 0.7
+    }
+    # I am aware that pluto and the moon are not a planet ;)
+    bodywaight = planets.get(body)
+    force = mass*bodywaight
+    force == mass * (planets.get(body))
+    return force
 
-# deel 2
-#1
-player = "Frank Rijkaard"
-#2
-first_name = player[:player.find(' ')]
-print(first_name)
-#3
-last_name_len = len(player [:player.find(' ')+3:])
-print (last_name_len)
-#4
-name_short = player[0] + '.' +  player[5:14]
-print(name_short)
-#5
-chant = len(first_name) * (first_name + '! ')
-#6
-chant = chant.rstrip( )
-print(chant)
+print(force(69))
 
-good_chant = (chant [-1] != " ")
-print(good_chant)
+ # Part 3: Gravity.
+
+ # pull(f) = G × ((m1×m2)/d2)
+
+def pull(m1, m2, d):
+    
+    forceconstant = (6.674 * 10**-11) 
+    pull = forceconstant * (m1 * m2)/(d**2)  
+
+    return pull
+
+print(pull(800, 1500, 3), 10)
